fa38abc9a869341ee1c861090f830332addeeaab
Kohei Ohtani <kouuver1@gmail.com>
2024-07-19 13:48:39 +0900

    Initial commit

--- init-db.sh
+++ init-db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # H2 データベースを起動し、スクリプトを実行
-java -cp /h2.jar org.h2.tools.RunScript -url jdbc:h2:file:/data/ecdb -user sa -password password -script /initdb/init.sql
+java -cp /h2.jar org.h2.tools.RunScript -url jdbc:h2:file:/initdb -user sa -password password -script /initdb/init.sql
