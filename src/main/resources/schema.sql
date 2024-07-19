b43c4def485d663296f66fa01a0802573ebb65ee
Kohei Ohtani <kouuver1@gmail.com>
2024-07-19 14:46:04 +0900

    Initial commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,96 +1,88 @@
-CREATE TABLE `m_users`
+CREATE TABLE m_users
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `user_name` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `furigana` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `e_mail` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `password` varchar (250) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `postal_code` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `prefectures` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `city` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `town_name` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `others` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-   `phone_number` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `gender` int NOT NULL,
-   `birthday` date NOT NULL,
-   `role` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `e_mail_UNIQUE` (`e_mail`)
+   id int NOT NULL IDENTITY,
+   user_name varchar (50) NOT NULL,
+   furigana varchar (50) NOT NULL,
+   e_mail varchar (50) NOT NULL UNIQUE,
+   password varchar (250) NOT NULL,
+   postal_code varchar (50) NOT NULL,
+   prefectures varchar (50) NOT NULL,
+   city varchar (50) NOT NULL,
+   town_name varchar (50) NOT NULL,
+   others varchar (50),
+   phone_number varchar (50) NOT NULL,
+   gender int NOT NULL,
+   birthday date NOT NULL,
+   role varchar (50) NOT NULL,
+   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
 );
-
-CREATE TABLE `m_goods`
+CREATE TABLE m_goods
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `goods_name` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `description` varchar (250) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `price` int NOT NULL,
-   `image_url` varchar (250) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `image_url_UNIQUE` (`image_url`)
+   id int NOT NULL IDENTITY,
+   goods_name varchar (50) NOT NULL,
+   description varchar (250) NOT NULL,
+   price int NOT NULL,
+   image_url varchar (250) NOT NULL UNIQUE,
+   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
 );
-
-CREATE TABLE `cart`
+CREATE TABLE cart
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `user_id` int NOT NULL,
-   `goods_id` int NOT NULL,
-   `quantity` varchar (50) NOT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
+   id int NOT NULL IDENTITY,
+   user_id int NOT NULL,
+   goods_id int NOT NULL,
+   quantity varchar (50) NOT NULL,
+   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
 );
-
 CREATE TABLE `order`
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `user_id` int NOT NULL,
-   `order_date` datetime NOT NULL,
-   `order_number` varchar (50) NOT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
+   id int NOT NULL IDENTITY,
+   user_id int NOT NULL,
+   order_date timestamp NOT NULL,
+   order_number varchar (50) NOT NULL,
+   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
 );
-
-CREATE TABLE `order_details`
+CREATE TABLE order_details
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `order_id` int NOT NULL,
-   `goods_id` int NOT NULL,
-   `price` int NOT NULL,
-   `quantity` int NOT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
+   id int NOT NULL IDENTITY,
+   order_id int NOT NULL,
+   goods_id int NOT NULL,
+   price int NOT NULL,
+   quantity int NOT NULL,
+   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
 );
-
-CREATE TABLE `delivery_address`
+CREATE TABLE delivery_address
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `order_id` int NOT NULL,
-   `postal_code` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `prefectures` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `city` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `town_name` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-   `others` varchar (50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
-   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
+   id int NOT NULL IDENTITY,
+   order_id int NOT NULL,
+   postal_code varchar (50) NOT NULL,
+   prefectures varchar (50) NOT NULL,
+   city varchar (50) NOT NULL,
+   town_name varchar (50) NOT NULL,
+   others varchar (50),
+   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   PRIMARY KEY (id)
 );
-
-CREATE TABLE `monthly_sales`
+CREATE TABLE monthly_sales
 (
-   `id` int NOT NULL AUTO_INCREMENT,
-   `month` int NOT NULL,
-   `year` int NOT NULL,
-   `sales` int NOT NULL,
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `uq_year_month`
+   id int NOT NULL IDENTITY,
+   month int NOT NULL,
+   year int NOT NULL,
+   sales int NOT NULL,
+   PRIMARY KEY (id),
+   CONSTRAINT uq_year_month UNIQUE
    (
-      `year`,
-      `month`
+      year,
+      month
    )
 );
